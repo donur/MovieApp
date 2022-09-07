7ce4a88f7ad8132b4f2900c866368a537ece12f1
Onur Dogan <iamonurdogan@gmail.com>
2022-09-07 10:16:01 +0300

    Bindings

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -12,4 +12,10 @@ export class MoviesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+movies = [
+  { id: 1, title: "Movie 1", description: "Adventure", imageUrl: "adventure-1.jpeg" },
+  { id: 2, title: "Movie 2", description: "Romance", imageUrl: "romance-1.jpeg"},
+  { id: 3, title: "Movie 3", description: "Drama", imageUrl: "drama.jpeg"},
+  { id: 4, title: "Movie 4", description: "Comedy", imageUrl: "comedy.jpeg"}
+]
 }
