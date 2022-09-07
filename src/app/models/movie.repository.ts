8856ab3e945d87b341e0411eecb505aa2a56608a
Onur Dogan <iamonurdogan@gmail.com>
2022-09-07 14:39:13 +0300

    popular movies

--- src/app/models/movie.repository.ts
+++ src/app/models/movie.repository.ts
@@ -5,15 +5,19 @@ export class MovieRepository {
 
   constructor() {
     this.movies = [
-      { id: 1, title: "Movie 1", description: "Adventure", imageUrl: "adventure-1.jpeg" },
-      { id: 2, title: "Movie 2", description: "Romance", imageUrl: "romance-1.jpeg"},
-      { id: 3, title: "Movie 3", description: "Drama", imageUrl: "drama.jpeg"},
-      { id: 4, title: "Movie 4", description: "Comedy", imageUrl: "comedy-1.jpeg"}
+      { id: 1, title: "Movie 1", description: "Adventure", imageUrl: "adventure-1.jpeg", isPopular: false },
+      { id: 2, title: "Movie 2", description: "Romance", imageUrl: "romance-1.jpeg", isPopular: false},
+      { id: 3, title: "Movie 3", description: "Drama", imageUrl: "drama.jpeg", isPopular: true},
+      { id: 4, title: "Movie 4", description: "Comedy", imageUrl: "comedy-1.jpeg", isPopular: true}
     ];
   }
 
   getMovies(): Movie[] {
     return this.movies;
+  }
+
+  getPopularMovies(): Movie[] {
+    return this.movies.filter(i => i.isPopular);
   }
 
   getMovieById(id: number): Movie {
