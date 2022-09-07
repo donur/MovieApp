6f87617e68a07680b1997f77ffe9c0ab10a6fe13
Onur Dogan <iamonurdogan@gmail.com>
2022-09-07 17:52:50 +0300

    custom pipe

--- src/app/models/movie.repository.ts
+++ src/app/models/movie.repository.ts
@@ -5,10 +5,10 @@ export class MovieRepository {
 
   constructor() {
     this.movies = [
-      { id: 1, title: "Movie 1", description: "Adventure", imageUrl: "adventure-1.jpeg", isPopular: false },
-      { id: 2, title: "Movie 2", description: "Romance", imageUrl: "romance-1.jpeg", isPopular: false},
-      { id: 3, title: "Movie 3", description: "Drama", imageUrl: "drama.jpeg", isPopular: true},
-      { id: 4, title: "Movie 4", description: "Comedy", imageUrl: "comedy-1.jpeg", isPopular: true}
+      { id: 1, title: "Movie 1", description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita amet fuga sit cum odio non fugit consequuntur incidunt voluptas architecto. Quos, cupiditate. Ipsa iure, accusantium dignissimos nostrum minima enim molestias!", imageUrl: "adventure-1.jpeg", isPopular: false, datePublished: new Date(1991,10,10) },
+      { id: 2, title: "Movie 2", description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita amet fuga sit cum odio non fugit consequuntur incidunt voluptas architecto. Quos, cupiditate. Ipsa iure, accusantium dignissimos nostrum minima enim molestias!", imageUrl: "romance-1.jpeg", isPopular: false, datePublished: new Date(1991,10,10)},
+      { id: 3, title: "Movie 3", description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita amet fuga sit cum odio non fugit consequuntur incidunt voluptas architecto. Quos, cupiditate. Ipsa iure, accusantium dignissimos nostrum minima enim molestias!", imageUrl: "drama.jpeg", isPopular: true, datePublished: new Date(1991,10,10)},
+      { id: 4, title: "Movie 4", description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita amet fuga sit cum odio non fugit consequuntur incidunt voluptas architecto. Quos, cupiditate. Ipsa iure, accusantium dignissimos nostrum minima enim molestias!", imageUrl: "comedy-1.jpeg", isPopular: true, datePublished: new Date(1991,10,10)}
     ];
   }
 
