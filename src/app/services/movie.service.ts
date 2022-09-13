6a2eba04bd1e60b792c8863155bfb386d1326d2f
Onur Dogan <iamonurdogan@gmail.com>
2022-09-13 10:26:40 +0300

    movie detail page

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,55 +1,54 @@
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable, throwError } from "rxjs";
-import { catchError, tap } from "rxjs/operators";
-import { Movie } from "../models/movie";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { Movie } from '../models/movie';
 
 @Injectable()
 export class MovieService {
-    url = "http://localhost:3000/movies";
+  url = 'http://localhost:3000/movies';
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    getMovies(categoryId: number): Observable<Movie[]> {
+  getMovies(categoryId: number): Observable<Movie[]> {
+    let newUrl = this.url;
 
-        let newUrl = this.url;
-
-        if(categoryId) {
-            newUrl += '?categoryId=' + categoryId;
-
-        }
-
-
-
-
-        return this.http.get<Movie[]>(newUrl).pipe(
-           tap(data => console.log(data)),
-           catchError(this.handleError)
-        );
+    if (categoryId) {
+      newUrl += '?categoryId=' + categoryId;
     }
 
-    private handleError(error: HttpErrorResponse) {
-        if(error.error instanceof ErrorEvent) {
-            // client or network 
-            console.log("error: " + error.error.message);
-        } else {
-            // api (backend)
-            switch(error.status) {
-                case 404:
-                    console.log("not found");
-                    break;
-                    console.log("access denied");
-                    break;
-                    case 500:
-                    console.log("interval server");
-                    break;
-                    default:
-                        console.log("Bilinmeyen bir hata");
-            }
+    return this.http.get<Movie[]>(newUrl).pipe(
+      tap((data) => console.log(data)),
+      catchError(this.handleError)
+    );
+  }
 
+  getMovieById(movieId: number): Observable<Movie> {
+    return this.http.get<Movie>(this.url + "/" + movieId).pipe(
+        tap(data => console.log(data)),
+        catchError(this.handleError)
+    );
+  }
 
-        }
-        return throwError("Bir hata oluştu."); // look throwError
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // client or network
+      console.log('error: ' + error.error.message);
+    } else {
+      // api (backend)
+      switch (error.status) {
+        case 404:
+          console.log('not found');
+          break;
+          console.log('access denied');
+          break;
+        case 500:
+          console.log('interval server');
+          break;
+        default:
+          console.log('Bilinmeyen bir hata');
+      }
     }
-
-}     
+    return throwError('Bir hata oluştu.'); // look throwError
+  }
+}
