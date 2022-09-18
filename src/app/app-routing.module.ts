b86061b6b486dae020a0d359af050d5f1445f81b
Onur Dogan <iamonurdogan@gmail.com>
2022-09-18 16:03:59 +0300

    movie form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieCreateComponent } from './movie-create/movie-create.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MoviesComponent } from './movies/movies.component';
 
@@ -7,6 +8,7 @@ const routes: Routes = [
   { path: 'movies', component: MoviesComponent },
   { path: '', redirectTo: 'movies', pathMatch: 'full' },
   { path: 'movies/category/:categoryId', component: MoviesComponent },
+  { path: 'movies/create', component: MovieCreateComponent },
   { path: 'movies/:movieId', component: MovieDetailsComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { MovieFilterPipe } from './pipes/movie-filter.pipe';
 import { AlertifyService } from './services/alertify.service';
 import { HttpClientModule } from '@angular/common/http';
+import { MovieCreateComponent } from './movie-create/movie-create.component';
 
 @NgModule({
   declarations: [ // components
@@ -26,6 +27,7 @@ import { HttpClientModule } from '@angular/common/http';
     FooterComponent,
     SummaryPipe,
     MovieFilterPipe,
+    MovieCreateComponent,
   ],
   imports: [ // module
     BrowserModule,
