ba8d1f3ff2782ff2896432452f14d9d556bc1c54
Onur Dogan <iamonurdogan@gmail.com>
2022-09-19 10:02:20 +0300

    checking form information

--- src/app/movie-create/movie-create.component.ts
+++ src/app/movie-create/movie-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Category } from '../models/category';
+import { AlertifyService } from '../services/alertify.service';
 import { CategoryService } from '../services/category.service';
 import { MovieService } from '../services/movie.service';
 
@@ -16,7 +17,9 @@ export class MovieCreateComponent implements OnInit {
   constructor(
     private categoryService: CategoryService,
     private movieService: MovieService,
-    private router: Router) {} // Create butonuna basıldığında movies list sayfasına yönlendirme yapar.
+    private router: Router, // Create butonuna basıldığında movies list sayfasına yönlendirme yapar.
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit(): void {
     this.categoryService.getCategories().subscribe((data) => {
@@ -25,6 +28,40 @@ export class MovieCreateComponent implements OnInit {
   }
 
   createMovie(title: any, description: any, imageUrl: any, categoryId: any) {
+    if (
+      title.value === '' ||
+      description.value === '' ||
+      imageUrl.value === '' ||
+      categoryId.value === '-1'
+    ) {
+      this.alertify.error('You must fill in all fields!');
+      return;
+    }
+
+    if (title.value.length < 5) {
+      this.alertify.error(
+        'You must enter a minimum of 5 characters for the title!'
+      );
+      return;
+    }
+
+    if (description.value.length < 10 || description.value.length > 50) {
+      this.alertify.error(
+        'For Description, you must enter a value in the range of 10-50 characters!'
+      );
+      return;
+    }
+
+    const imgExtensions = ['jpeg', 'jpg', 'png'];
+    const imgExtension = imageUrl.value.split('.').pop();
+
+    if (imgExtensions.indexOf(imgExtension) === -1) {
+      this.alertify.error(
+        "You can only add images with 'jpeg', 'jpg', 'png' extensions!"
+      );
+      return;
+    }
+
     const movie = {
       id: 0,
       title: title.value,
@@ -35,8 +72,8 @@ export class MovieCreateComponent implements OnInit {
       categoryId: categoryId.value,
     };
 
-    this.movieService.createMovie(movie).subscribe(data =>
-      this.router.navigate(['/movies', data.id]) // Create butonuna basıldığında movies list sayfasına yönlendirme yapar.
-      );
+    this.movieService.createMovie(movie).subscribe(
+      (data) => this.router.navigate(['/movies', data.id]) // Create butonuna basıldığında movies list sayfasına yönlendirme yapar.
+    );
   }
 }
